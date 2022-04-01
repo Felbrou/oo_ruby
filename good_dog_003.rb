aa7725ce7ba210ef2d49f7d0d3496426577f60a2
Felbrou <15351171+Felbrou@users.noreply.github.com>
2022-04-01 18:17:22 -0300

    config code

--- good_dog_003.rb
+++ good_dog_003.rb
@@ -2,9 +2,16 @@
 
 class GoodDog
 	def initialize(name)
-		@name = name # the @ is called instance variable, it is a variable that exists as long as long as the object instance exists
-	end              # and that is one of the ways we tie data to objects
-end
+		@name = name # the @ is called instance variable, it is a variable that exists as long as long as the object instance exists and that is one of the ways we tie data to objects
+	end
+
+	def speak
+	"Arf!"
+	end
+end           
+
 
 sparky = GoodDog.new("Sparky") # here the string "Sparky" is being passed from the 'new' method through to the initialize method
                                # Within the constructor (i.e., the initialize method), we then set the instance variable @name to name, which results in assigning the string "Sparky" to the @name instance variable.
+
+puts sparky.speak
